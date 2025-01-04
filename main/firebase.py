ff98075b5542d33c00bb2d236e60cc38d6dba2a2
daryl <watanaprasit@gmail.com>
2025-01-04 08:53:29 +0800

    deploy on render

--- main/firebase.py
+++ main/firebase.py
@@ -3,34 +3,26 @@ from firebase_admin import credentials, firestore
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 
-# Initialize Firebase
 
 cred = credentials.Certificate(os.getenv('FIREBASE_KEY_PATH'))
-
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
-# Firestore client reference
 db = firestore.client()
 
-# Firestore initialization for the counters document
 def initialize_counter():
     counter_ref = db.collection('counters').document('production_forecasts_counter')
     
-    # Check if the counter document exists
     doc = counter_ref.get()
     
     if not doc.exists:
-        # Initialize the counter document with an ID of 1 if it doesn't exist
         counter_ref.set({
             'current_id': 1
         })
     else:
-        # If it exists, log the current value for debugging (optional)
         print(f"Counter document exists. Current ID: {doc.to_dict()['current_id']}")
         
 def initialize_asset_locations_counter():
